1a2a67e19c45673a32ff02d487907000d5cc71a1
megiahmetaj <megiahmetaj2005@gmail.com>
2025-08-09 11:45:26 +0200

    Initial commit

---
+++ OnlineStoreProject/src/main/java/com/example/online_store_project/OnlineStoreProject/OnlineStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_store_project.OnlineStoreProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreProjectApplication.class, args);
+	}
+
+}
