12d336422fbeac6a63c3e0a186a61101dbc19003
megiahmetaj <megiahmetaj2005@gmail.com>
2025-08-09 11:45:26 +0200

    Initial commit

---
+++ OnlineStoreProject/src/test/java/com/example/online_store_project/OnlineStoreProject/OnlineStoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_store_project.OnlineStoreProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
